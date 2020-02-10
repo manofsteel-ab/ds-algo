2ea990bd5e72a75c6c4c43d8fe3e0bb1bcab2d35
Abhishek Kumar Yadav <abhiy.yadav7@gmail.com>
2020-02-11 00:17:37 +0530

    sm

--- BinaryTree/path_sum_paths.py
+++ BinaryTree/path_sum_paths.py
@@ -1,4 +1,4 @@
-# Definition for a  binary tree node
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
@@ -10,19 +10,20 @@ class Solution:
     # @param B : integer
     # @return a list of list of integers
 
-    def _getPaths(self, root, total, path, res):
+    def _getPaths(self, root, total, sum_so_far, path, res):
         if not root:
             return
 
         path.append(root.val)
+        sum_so_far += root.val
 
-        if sum(path) == total and not (root.left or root.right):
+        if sum_so_far == total and not (root.left or root.right):
             res.append(path[:])
-        self._getPaths(root.left, total, path, res)
-        self._getPaths(root.right, total, path, res)
+        self._getPaths(root.left, total, sum_so_far, path, res)
+        self._getPaths(root.right, total, sum_so_far, path, res)
         path.pop()
 
     def pathSum(self, root, sum_val):
         res = []
-        self._getPaths(root, sum_val, [], res)
+        self._getPaths(root, sum_val, 0, [], res)
         return res
