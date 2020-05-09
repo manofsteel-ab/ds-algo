89e6612c979309ba3d89e888d0d07cc0daf944a8
Abhishek Kr Yadav <abhiy.yadav7@gmail.com>
2020-05-10 00:53:25 +0530

    newwork delay using dijkstra

--- problem-solving-leetcode/graph/network_delay_time.py
+++ problem-solving-leetcode/graph/network_delay_time.py
@@ -14,10 +14,10 @@ class Solution:
         for u,v,w in weighted_matrix:
             self.graph[u].append((v,w))
 
-    def _dfs(self, src, destination_distance: dict):
-        pass
+    def _bfs(self, N, src):
 
-    def _bfs(self, src, destination_distance: dict):
+        destination_distance = {node:float('inf') for node in range(1, N+1)}
+        destination_distance[src] = 0
 
         queue = [src]
 
@@ -31,6 +31,34 @@ class Solution:
                     )
                     queue.append(v)
 
+        ans = max(destination_distance.values())
+        if ans<float(inf):
+            return ans
+        return -1
+
+    def _dijkstra(self, N, src):
+
+        pq = [(0, src)]
+
+        destination_distance = {}
+
+        while pq:
+
+            w,u = heapq.heappop(pq)
+
+            if u in destination_distance:
+                continue
+
+            destination_distance[u] = w
+
+            for v,w in self.graph[u]:
+                if v not in destination_distance:
+                    heapq.heappush(pq, (destination_distance[u]+w, v))
+        # print(destination_distance.values())
+        if len(destination_distance.values()) == N:
+            return max(destination_distance.values())
+        return -1
+
 
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
 
@@ -42,12 +70,4 @@ class Solution:
         if K not in self.graph:
             return -1
 
-        destination_distance = {node:float('inf') for node in range(1, N+1)}
-        destination_distance[K] = 0
-
-        self._bfs(K, destination_distance)
-
-        ans = max(destination_distance.values())
-        if ans<float(inf):
-            return ans
-        return -1
+        return self._dijkstra(N, K)
